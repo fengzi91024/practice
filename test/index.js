bbc4cc12775461d247fbf566c7da466901b8246e
huangyilin <180584517@qq.xom>
2023-12-06 11:29:37 +0800

    重新构建数据库

--- test/index.js
+++ test/index.js
@@ -1,40 +1,59 @@
-const {Sequelize, DataTypes, Model} = require('sequelize');
-
-
+const { Sequelize, DataTypes, Model } = require("sequelize");
 
 /* 
     模型实例
     Model Instances
 */
 
-
 // 连接数据库
-const sequelize = new Sequelize('test','root','p@ssw0rd',{
-    host:'localhost',
-    dialect:'mysql',
-    
-    define:{
-        freezeTableName:true,    //全局配置停止模型名称复数自动化
-        noPrimaryKay:true    // 防止Sequelize 自动将主键属性id 添加到每个模型
-    }
+const sequelize = new Sequelize("test", "root", "p@ssw0rd", {
+  host: "localhost",
+  dialect: "mysql",
+  define:{
+    timestamps:false
+  }
 });
 
 
+async function test(){
+    try{
+        await sequelize.authenticate();
+        console.log('Connection has been establicshed successfully');
+    } catch(err){
+        console.error('Unable to connect to the databases:',err);
+    }
+
+}
+
+test();
 
 //创建模型
-const User = sequelize.define("user",{
-    name:DataTypes.TEXT,
-    favoriteColor:{
-        type:DataTypes.TEXT,
-        defaultValue:'green'
-    },
-    age:DataTypes.INTEGER,
-    cash:DataTypes.INTEGER
+const User = sequelize.define("user", {
+  name: DataTypes.TEXT,
+  favoriteColor: {
+    type: DataTypes.TEXT,
+    defaultValue: "green",
+  },
+  age: DataTypes.INTEGER,
+  cash: DataTypes.INTEGER
 });
 
+const jane = User.build({ name: "Jane" });
+console.log(jane instanceof User);
+console.log(jane.name);
 
-// 同步数据
+
+
+
+//创建实列   尽管模型是一个类，但是不是用new运算符创建实列，应该使用build 方法：
+
+
 (async () => {
+    // 同步数据库（等待操作完成）
     await sequelize.sync({ force: true });
-    // 这里是代码
+  
+    // 创建并保存实例（等待操作完成）
+    await jane.save();
+    console.log('Jane 已保存到数据库!');
   })();
+  
